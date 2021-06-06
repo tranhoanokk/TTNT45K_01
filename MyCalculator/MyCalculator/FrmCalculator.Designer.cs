964d4abe8428d1a59cf243e6952f1c7265662aa4
Ngocnguyen01 <ngocnguyennt01@gmail.com>
2021-06-06 22:36:27 +0700

    submit

---
+++ MyCalculator/MyCalculator/FrmCalculator.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace MyCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtSo1 = new System.Windows.Forms.TextBox();
+            this.txtSo2 = new System.Windows.Forms.TextBox();
+            this.txtKQ = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSo1
+            // 
+            this.txtSo1.Location = new System.Drawing.Point(72, 43);
+            this.txtSo1.Name = "txtSo1";
+            this.txtSo1.Size = new System.Drawing.Size(100, 20);
+            this.txtSo1.TabIndex = 0;
+            this.txtSo1.TextChanged += new System.EventHandler(this.txtSo1_TextChanged);
+            // 
+            // txtSo2
+            // 
+            this.txtSo2.Location = new System.Drawing.Point(302, 43);
+            this.txtSo2.Name = "txtSo2";
+            this.txtSo2.Size = new System.Drawing.Size(100, 20);
+            this.txtSo2.TabIndex = 1;
+            this.txtSo2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtKQ
+            // 
+            this.txtKQ.Location = new System.Drawing.Point(186, 95);
+            this.txtKQ.Name = "txtKQ";
+            this.txtKQ.ReadOnly = true;
+            this.txtKQ.Size = new System.Drawing.Size(100, 20);
+            this.txtKQ.TabIndex = 2;
+            // 
+            // btnCong
+            // 
+            this.btnCong.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCong.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnCong.Location = new System.Drawing.Point(43, 162);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 29);
+            this.btnCong.TabIndex = 3;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = false;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnTru.Location = new System.Drawing.Point(148, 162);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(75, 29);
+            this.btnTru.TabIndex = 4;
+            this.btnTru.Text = "Trừ";
+            this.btnTru.UseVisualStyleBackColor = false;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnNhan.Location = new System.Drawing.Point(251, 162);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(75, 29);
+            this.btnNhan.TabIndex = 5;
+            this.btnNhan.Text = "Nhân";
+            this.btnNhan.UseVisualStyleBackColor = false;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnChia.Location = new System.Drawing.Point(358, 162);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(75, 29);
+            this.btnChia.TabIndex = 6;
+            this.btnChia.Text = "Chia";
+            this.btnChia.UseVisualStyleBackColor = false;
+            this.btnChia.Click += new System.EventHandler(this.btnChia_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(466, 279);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.txtKQ);
+            this.Controls.Add(this.txtSo2);
+            this.Controls.Add(this.txtSo1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "MyCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSo1;
+        private System.Windows.Forms.TextBox txtSo2;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+    }
+}
+
---
+++ MyCalculator/MyCalculator/FrmCalculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void txtSo1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(txtSo1.Text) || checkNumIsEmpty(txtSo2.Text))
+            {
+                MessageBox.Show("Có một số chưa nhập, vui lòng nhập lại", "Thông báo");
+                return;
+            }
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ=dSo1+dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(txtSo1.Text) || checkNumIsEmpty(txtSo2.Text))
+            {
+                MessageBox.Show("Có một số chưa nhập, vui lòng nhập lại", "Thông báo");
+                return;
+            }
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 - dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(txtSo1.Text) || checkNumIsEmpty(txtSo2.Text))
+            {
+                MessageBox.Show("Có một số chưa nhập, vui lòng nhập lại", "Thông báo");
+                return;
+            }
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            decimal dKQ = dSo1 * dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(txtSo1.Text) || checkNumIsEmpty(txtSo2.Text))
+            {
+                MessageBox.Show("Có một số chưa nhập, vui lòng nhập lại", "Thông báo");
+                return;
+            }
+            string sSo1 = txtSo1.Text;
+            decimal dSo1 = Convert.ToDecimal(sSo1);
+            string sSo2 = txtSo2.Text;
+            decimal dSo2 = Convert.ToDecimal(sSo2);
+            if (dSo2 == 0)
+            {
+                MessageBox.Show("Vui lòng nhập số thứ hai khác 0", "Thông báo");
+                return;
+            }
+            decimal dKQ = dSo1 / dSo2;
+            txtKQ.Text = dKQ.ToString();
+        }
+            private bool checkNumIsEmpty(String str)
+        {
+            if (str == "")
+            {
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
