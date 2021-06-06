0d2a6908b6001db5793094705e600e927af1e4cc
Ngocnguyen01 <ngocnguyennt01@gmail.com>
2021-06-06 15:49:31 +0700

    Calculator

---
+++ Calculator_nhom1/Calculator_nhom1/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Calculator_nhom1
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.so1 = new System.Windows.Forms.TextBox();
+            this.so2 = new System.Windows.Forms.TextBox();
+            this.ketqua = new System.Windows.Forms.TextBox();
+            this.btnCong = new System.Windows.Forms.Button();
+            this.btnTru = new System.Windows.Forms.Button();
+            this.btnNhan = new System.Windows.Forms.Button();
+            this.btnChia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // so1
+            // 
+            this.so1.Location = new System.Drawing.Point(68, 66);
+            this.so1.Name = "so1";
+            this.so1.Size = new System.Drawing.Size(100, 20);
+            this.so1.TabIndex = 0;
+            // 
+            // so2
+            // 
+            this.so2.Location = new System.Drawing.Point(274, 66);
+            this.so2.Name = "so2";
+            this.so2.Size = new System.Drawing.Size(100, 20);
+            this.so2.TabIndex = 1;
+            // 
+            // ketqua
+            // 
+            this.ketqua.Location = new System.Drawing.Point(175, 117);
+            this.ketqua.Name = "ketqua";
+            this.ketqua.ReadOnly = true;
+            this.ketqua.Size = new System.Drawing.Size(100, 20);
+            this.ketqua.TabIndex = 2;
+            // 
+            // btnCong
+            // 
+            this.btnCong.Location = new System.Drawing.Point(42, 179);
+            this.btnCong.Name = "btnCong";
+            this.btnCong.Size = new System.Drawing.Size(75, 23);
+            this.btnCong.TabIndex = 3;
+            this.btnCong.Text = "Cộng";
+            this.btnCong.UseVisualStyleBackColor = true;
+            this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
+            // 
+            // btnTru
+            // 
+            this.btnTru.Location = new System.Drawing.Point(137, 179);
+            this.btnTru.Name = "btnTru";
+            this.btnTru.Size = new System.Drawing.Size(75, 23);
+            this.btnTru.TabIndex = 4;
+            this.btnTru.Text = "Trừ";
+            this.btnTru.UseVisualStyleBackColor = true;
+            this.btnTru.Click += new System.EventHandler(this.btnTru_Click);
+            // 
+            // btnNhan
+            // 
+            this.btnNhan.Location = new System.Drawing.Point(232, 179);
+            this.btnNhan.Name = "btnNhan";
+            this.btnNhan.Size = new System.Drawing.Size(75, 23);
+            this.btnNhan.TabIndex = 5;
+            this.btnNhan.Text = "Nhân";
+            this.btnNhan.UseVisualStyleBackColor = true;
+            this.btnNhan.Click += new System.EventHandler(this.btnNhan_Click);
+            // 
+            // btnChia
+            // 
+            this.btnChia.Location = new System.Drawing.Point(327, 179);
+            this.btnChia.Name = "btnChia";
+            this.btnChia.Size = new System.Drawing.Size(75, 23);
+            this.btnChia.TabIndex = 6;
+            this.btnChia.Text = "Chia";
+            this.btnChia.UseVisualStyleBackColor = true;
+            this.btnChia.Click += new System.EventHandler(this.btnChia_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 298);
+            this.Controls.Add(this.btnChia);
+            this.Controls.Add(this.btnNhan);
+            this.Controls.Add(this.btnTru);
+            this.Controls.Add(this.btnCong);
+            this.Controls.Add(this.ketqua);
+            this.Controls.Add(this.so2);
+            this.Controls.Add(this.so1);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox so1;
+        private System.Windows.Forms.TextBox so2;
+        private System.Windows.Forms.TextBox ketqua;
+        private System.Windows.Forms.Button btnCong;
+        private System.Windows.Forms.Button btnTru;
+        private System.Windows.Forms.Button btnNhan;
+        private System.Windows.Forms.Button btnChia;
+    }
+}
+
---
+++ Calculator_nhom1/Calculator_nhom1/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_nhom1
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCong_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(so1.Text) || checkNumIsEmpty(so2.Text))
+            {
+                MessageBox.Show("Co mot so chua nhap, vui long nhap lai");
+                return;
+            }
+
+            double s1 = double.Parse(so1.Text);
+            double s2 = double.Parse(so2.Text);
+            ketqua.Text = (s1 + s2).ToString();
+        }
+
+        private void so1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // chỉ cho phép nhập kí tự số
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void so2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnTru_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(so1.Text) || checkNumIsEmpty(so2.Text))
+            {
+                MessageBox.Show("Co mot so chua nhap, vui long nhap lai");
+                return;
+            }
+
+            double s1 = double.Parse(so1.Text);
+            double s2 = double.Parse(so2.Text);
+            ketqua.Text = (s1 - s2).ToString();
+        }
+
+        private void btnNhan_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(so1.Text) || checkNumIsEmpty(so2.Text))
+            {
+                MessageBox.Show("Co mot so chua nhap, vui long nhap lai");
+                return;
+            }
+
+            double s1 = double.Parse(so1.Text);
+            double s2 = double.Parse(so2.Text);
+            ketqua.Text = (s1 * s2).ToString();
+        }
+
+        private void btnChia_Click(object sender, EventArgs e)
+        {
+            if (checkNumIsEmpty(so1.Text) || checkNumIsEmpty(so2.Text))
+            {
+                MessageBox.Show("Co mot so chua nhap, vui long nhap lai");
+                return;
+            }
+
+            double s1 = double.Parse(so1.Text);
+            double s2 = double.Parse(so2.Text);
+
+            if (s2 == 0)
+            {
+                MessageBox.Show("Nhap so thu hai khac 0");
+                return;
+            }
+
+            ketqua.Text = (s1 / s2).ToString();
+        }
+        private bool checkNumIsEmpty(String str)
+        {
+            if (str == "")
+            {
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
